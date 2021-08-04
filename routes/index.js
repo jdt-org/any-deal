720d2d42eecca161a2fe8af6b467aab76f760c45
Derek M. Robertson <dmr.webdev@gmail.com>
2021-08-03 17:39:28 -0700

    Corrected query method, truncated url

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,23 @@ var router = express.Router();
 var fetch = require('node-fetch');
 
 
+/* router.get('/favicon.ico', (req, res) => {
+  return
+}) */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function (req, res) {
-  const { subreddit } = req.body
+router.get('/results', function (req, res) {
+  const { subreddit } = req.query;
   fetch(`https://api.reddit.com/r/${subreddit}/hot?limit=100`)
     .then(res => res.json())
     .then(data => {
       const subResults = data.data.children
       res.render('results', { subResults })
     })
-  
-  //res.json()
 })
 
 module.exports = router;
